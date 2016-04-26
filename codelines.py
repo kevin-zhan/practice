277c16c82babc261fd734b1ace711049b8a71fb7
KevinZhan <kevinzhan0417@outlook.com>
2016-04-26 17:47:54 +0800

    完成统计提交代码行数的脚本

---
+++ codelines.py
@@ -0,0 +1,48 @@
+#coding=utf8
+import os
+import time 
+import string
+filename = "code_record"
+#获取上次运行此脚本的时间
+file = open(filename,"r")
+pre_time = file.readline()
+pre_time = pre_time[19:35]
+file.close()
+#更新运行此脚本的时间
+now_time = time.strftime('%Y-%m-%d-%H:%M',time.localtime(time.time()))
+file = open(filename,'r+')
+file.write("上次记录时间:" + now_time + "\n")
+file.close()
+
+#计算自上一次运行此脚本以来，提交了多少行代码
+cmd = "git log --shortstat --since==" + pre_time
+output = os.popen(cmd)
+content = output.read()
+commits = content.split("commit") #用"commit"作关键字分隔每次提交的commit
+add_line = []
+sub_line = []
+for commit in commits[1:]:
+	str1 = "changed, "
+	str2 = " insertions(+)"
+	str3 = " deletions(-)"
+	pos1 = commit.find(str1)+len(str1)
+	pos2 = commit.find(str2)
+	pos3 = commit.find(str3)
+	if pos1<0:
+		continue
+	if pos2<0:
+		add_line.append(0)
+		sub_line.append(string.atoi(commit[pos1:pos3]))
+		continue
+	add_line.append(string.atoi(commit[pos1:pos2]))
+	pos2 = pos2 + len(str2)+2
+	if pos3<0:
+		sub_line.append(0)
+		continue
+	sub_line.append(string.atoi(commit[pos2:pos3]))
+code_line = sum(add_line)-sum(sub_line)
+if code_line != 0:	
+	file = open(filename,'a+')
+	file.write(now_time + "::::%d\n"%code_line)
+	file.close()
+print "统计完成，自" + pre_time + "开始，到现在(" + now_time + ")共添加了%d行代码。"%code_line
